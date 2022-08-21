ede41aec1567c0dc8877b8b8a309f1c9f3c1af62
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2022-08-21 12:24:24 -0400

    test

--- components/Desktop/Layouts/layoutAutoCarrousel.tsx
+++ components/Desktop/Layouts/layoutAutoCarrousel.tsx
@@ -1,13 +1,19 @@
 import style from "/styles/Desktop/Layouts/layoutAutoCarrousel.module.css"
 import Image from "next/image";
 import React, {useEffect, useState} from "react";
-
 const renderContainer: string = "renderContainerAutoCar"
-const widthDiv = 50;
+const widthDiv = 30;
 
 export default function LayoutAutoCarrousel({gapLayout, listImages}:
                                                 { gapLayout: number, listImages: string[] }) {
     let [stateImages, setStateImages] = useState(listImages)
+    const addItem = ()=>{
+        const renderDiv = document.getElementById(renderContainer)
+        const newDiv = <div className={style.testImage}>
+            <Image layout={"fill"} src={listImages[0]} alt=""/>
+        </div>
+        renderDiv.appendChild(renderDiv.firstChild)
+    }
     let [start, setStart] = useState("translate(-0px)")
     const handleSetStart = () => setCounter(counter = counter - 1)
     let [counter, setCounter] = useState(0)
@@ -20,19 +26,18 @@ export default function LayoutAutoCarrousel({gapLayout, listImages}:
         const interval = setInterval(handleMove, 1000)
         return () => clearInterval(interval)
     })
-
     return (
         <div className="overflow-hidden w-full">
             <div id={renderContainer} style={{gap: gapLayout, transform: start}} className={style.gridCarrouselAuto}>
                 {listImages.map((image: string, index) =>
                     <>
-                        <div className={style.testImage}>
+                    <div key={index} className={style.testImage}>
+                        <Image layout={"fill"} src={image} alt=""/>
+                    </div>
+                        <div key={index} className={style.testImage}>
                             <Image layout={"fill"} src={image} alt=""/>
                         </div>
-                        <div className={style.testImage}>
-                            <Image layout={"fill"} src={image} alt=""/>
-                        </div>
-                        <div className={style.testImage}>
+                        <div key={index} className={style.testImage}>
                             <Image layout={"fill"} src={image} alt=""/>
                         </div>
                     </>
