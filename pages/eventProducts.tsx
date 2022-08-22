922b8e4812d568231c2fc9ea27a15eee4cb8b6ab
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2022-08-22 19:59:49 -0400

    test

--- pages/eventProducts.tsx
+++ pages/eventProducts.tsx
@@ -234,7 +234,7 @@ export default function EventProducts() {
                         <>
                             <HeaderSpixMobile displaySug={handleIsDisplaySug} isDarkMode={isDarkMode}/>
                             <MenuSpixMobile listItemMenu={menuList} isDarkMode={isDarkMode}/>
-                            <div>prueba fixed1</div>
+                            <div>prueba fixed2</div>
                             <div className={styleMob.heightCont}
                                  style={{height: heightDiv}}>
                                 <ContSelectedProduct isOpen={isOpenSelectedProduct}
