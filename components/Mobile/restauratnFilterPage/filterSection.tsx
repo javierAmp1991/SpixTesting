ca42fba9646eec3cb8529c026a89c50507d9078f
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2022-08-29 22:37:57 -0400

    test

--- components/Mobile/restauratnFilterPage/filterSection.tsx
+++ components/Mobile/restauratnFilterPage/filterSection.tsx
@@ -1,44 +1,137 @@
 import Image from "next/image";
-import React from "react";
+import React, {useState} from "react";
 import {GlobalConst} from "../../../public/globalConst";
-const listFilters: string[] = ["filtro1", "filtro2", "filtro3", "filtro4", "filtro5", "filtro6", "filtro7", "filtro8", "filtro9"]
+class FilterProps{
+    Id: string
+    ImagePath: string
+    Name: string
+    IsSelected: boolean
+}
+const listFilters: FilterProps[] = [
+    {
+        Id: "abcd",
+        ImagePath: GlobalConst.sourceImages.ratingIndFull,
+        Name: "Sushi",
+        IsSelected: false
+    },
+    {
+        Id: "abcde",
+        ImagePath: GlobalConst.sourceImages.ratingIndFull,
+        Name: "Pizza",
+        IsSelected: false
+    },
+    {
+        Id: "abcdef",
+        ImagePath: GlobalConst.sourceImages.ratingIndFull,
+        Name: "Hamburguesa",
+        IsSelected: false
+    },
+    {
+        Id: "abcdefg",
+        ImagePath: GlobalConst.sourceImages.ratingIndFull,
+        Name: "Peruana",
+        IsSelected: false
+    },
+    {
+        Id: "abcdefgh",
+        ImagePath: GlobalConst.sourceImages.ratingIndFull,
+        Name: "Cafe",
+        IsSelected: false
+    },
+    {
+        Id: "abcdefgh12",
+        ImagePath: GlobalConst.sourceImages.ratingIndFull,
+        Name: "Almuerzos",
+        IsSelected: false
+    },
+    {
+        Id: "abcdef12345",
+        ImagePath: GlobalConst.sourceImages.ratingIndFull,
+        Name: "Desayunos",
+        IsSelected: false
+    },
+    {
+        Id: "abcdefg12345kl",
+        ImagePath: GlobalConst.sourceImages.ratingIndFull,
+        Name: "Alcohol",
+        IsSelected: false
+    },
+]
+
+const principalFilterList: FilterProps[] = [
+    {
+        Id: "1234abcd",
+        ImagePath: GlobalConst.sourceImages.ratingIndFull,
+        Name: "Ofertas",
+        IsSelected: false
+    },
+    {
+        Id: "12345abcde",
+        ImagePath: GlobalConst.sourceImages.ratingIndFull,
+        Name: "Menor a Mayor",
+        IsSelected: false
+    },
+]
+
+
 import style from "/styles/Mobile/RestaurantFilterPage/filterSection.module.css"
+import utilities from "/styles/utilities.module.css";
 
 export default function FilterSection() {
+    let[atributesFilters, setAtributesFilters] = useState(listFilters)
+    const handleClickFilter = (idFilter: string, isClicked: boolean) => {
+        const newListFilter = atributesFilters.map(item =>{
+            if(item.Id == idFilter){
+                return {...item, IsSelected: isClicked}
+            }
+            else return item
+        })
+        setAtributesFilters(atributesFilters = newListFilter)
+    }
+    let[principalFilters, setPrincipalFilters] = useState(principalFilterList)
+    const handleClickPrincipalFilters = (idFilter: string, isClicked: boolean) => {
+        const newListFilter = principalFilters.map(item =>{
+            if(item.Id == idFilter){
+                return {...item, IsSelected: isClicked}
+            }
+            else return item
+        })
+        setPrincipalFilters(principalFilters = newListFilter)
+    }
     return (
         <div>
             <div className={style.carrouselCont}>
                 {
-                    listFilters.map((item, index) =>
-                        <div key={index} className={style.gridFilterImage}>
+                    atributesFilters.map((item, index) =>
+                        <button onClick={() => handleClickFilter(item.Id, !item.IsSelected)}
+                                key={item.Id}
+                                className={item.IsSelected? style.gridFilterImageSelected : style.gridFilterImage}>
                             <div className={style.imageSize}>
-                                <Image layout={"fill"} src={GlobalConst.sourceImages.ratingIndFull} alt={""}/>
+                                <Image layout={"fill"} src={item.ImagePath} alt={""}/>
                             </div>
-                            <div className={"mt-2"}>
-                                {item}
+                            <div className={`${utilities.fontPrimarText} ${style.textFilter}`}>
+                                {item.Name}
                             </div>
-                        </div>
+                        </button>
                     )
                 }
             </div>
 
             <div className={style.buttonsCont}>
-                <div className={style.gridButton}>
-                    <div className={style.imageSize}>
-                        <Image layout={"fill"} src={GlobalConst.sourceImages.ratingIndFull} alt={""}/>
-                    </div>
-                    <div>
-                        Filtro1
-                    </div>
-                </div>
-                <div className={style.gridButton}>
-                    <div className={style.imageSize}>
-                        <Image layout={"fill"} src={GlobalConst.sourceImages.ratingIndFull} alt={""}/>
-                    </div>
-                    <div>
-                        Filtro2
-                    </div>
-                </div>
+                {
+                    principalFilters.map((item, index) =>
+                        <div onClick={() => handleClickPrincipalFilters(item.Id, !item.IsSelected)}
+                             className={item.IsSelected? style.gridButton : style.gridButtonSelected}>
+                            <div className={style.imageSize}>
+                                <Image layout={"fill"} src={item.ImagePath} alt={""}/>
+                            </div>
+                            <div className={utilities.fontPrimarText}>
+                                {item.Name}
+                            </div>
+                        </div>
+                    )
+                }
+
             </div>
         </div>
     )
