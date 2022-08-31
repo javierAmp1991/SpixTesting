c52a719429869bc517760b75e9ed92a140a7f061
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2022-08-31 13:41:49 -0400

    test

--- components/Mobile/Misc/headerSpixMobile.tsx
+++ components/Mobile/Misc/headerSpixMobile.tsx
@@ -4,15 +4,18 @@ import Image from "next/image";
 
 const styleSearchBar: boolean = false
 import {GlobalConst} from "../../../public/globalConst";
+import Link from "next/link";
 
 export default function HeaderSpixMobile({isDarkMode, displaySug, isDesplegable}) {
     let cssStyles = getCssStyles()
     return (
         <div className={`${cssStyles.bgHeader} ${style.gridHeader}`}>
             <div className={style.gridLogoBar}>
+                <Link href={"/"}>
                 <div className={style.sizeLogoSpix}>
                     <Image layout={"fill"} src={GlobalConst.sourceImages.spixAloneWhite}/>
                 </div>
+                </Link>
                 <SearchBarMobile isSug={false}
                                  styleSearchBar={styleSearchBar}
                                  isDisplaySug={displaySug}
