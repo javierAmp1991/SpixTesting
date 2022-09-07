87c37ab435d3f5ce9bf7e68bb4e86ecdc3d27c36
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2022-09-06 21:46:33 -0300

    test

--- components/Desktop/Search/newSearcPrincipalDesktop.tsx
+++ components/Desktop/Search/newSearcPrincipalDesktop.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import style from "/styles/Desktop/Search/newsSearch.module.css"
 import utilities from "/styles/utilities.module.css";
 import {News} from "../../../dataDemo/data";
+import {GlobalConst} from "../../../public/globalConst";
 
 export default function NewSearcPrincipalDesktop({item}: { item: News }) {
     return (
@@ -12,16 +13,18 @@ export default function NewSearcPrincipalDesktop({item}: { item: News }) {
                         <Image layout={"fill"} objectFit={"cover"} src={item.PathImage} alt={""}/>
                     </div>
                     <div className={style.gridInfo}>
-                        <div className={`${utilities.fontTitle}`}>
-                            {item.Title}
+                        <div className={style.grid1}>
+                            <div className={`${utilities.fontTitle}`}>
+                                {item.Title}
+                            </div>
+                            <div className={utilities.fontPrimaryText}>
+                                Evento: {item.Event}
+                            </div>
+                            <div className={utilities.fontPrimaryText}>
+                                Fecha: {item.Date}
+                            </div>
                         </div>
-                        <div className={utilities.fontPrimaryText}>
-                            Evento: {item.Event}
-                        </div>
-                        <div className={utilities.fontPrimaryText}>
-                            Fecha: {item.Date}
-                        </div>
-                        <div className={`${utilities.fontPrimaryText} ${utilities.clamp3}`}>
+                        <div className={`${utilities.fontPrimaryText} ${utilities.clamp4}`}>
                             {item.SubTitle}
                         </div>
                         <div className={`${utilities.styleLink} ${style.writeNewPrincipal}`}>
