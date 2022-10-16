d26bcbcc56d154d1fb8040063f2e090547d1bfef
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2022-10-15 21:40:02 -0300

    test

--- components/Mobile/userAccount/AccountSectionOptionMobile.tsx
+++ components/Mobile/userAccount/AccountSectionOptionMobile.tsx
@@ -2,14 +2,12 @@ import style from "/styles/Desktop/UserAccount/accountSectionOption.module.css"
 import {AccountSections, ProviderAccountSections, AccountSectionContext} from "../../Providers/providerUserAccount";
 import Image from "next/image";
 import {useContext, useEffect, useState} from "react";
-import Link from "next/link";
 
 export default function AccountSectionOptionMobile({item}: { item: AccountSections }) {
     const accountSectionContext: ProviderAccountSections = useContext(AccountSectionContext)
     const handleSelectSection = (id: string) => {
         accountSectionContext.SelectSection(id)
     }
-
     let [stateMyBussines, setStateMyBussines] = useState(false)
     /*useEffect(() => {
         accountSectionContext.ListAccountSection.forEach(item => {
@@ -21,18 +19,16 @@ export default function AccountSectionOptionMobile({item}: { item: AccountSectio
     }, [accountSectionContext.ListAccountSection])*/
 
     return (
-        <Link href={"/accountSection"}>
-            <button onClick={() => handleSelectSection(item.Id)}
-                    className={item.State ? style.mainDivSelected : style.mainDiv}>
-                <div className={style.sizeImage}>
-                    <Image layout={"fill"} src={item.PathImage}/>
+        <button onClick={() => handleSelectSection(item.Id)}
+                className={item.State ? style.mainDivSelected : style.mainDiv}>
+            <div className={style.sizeImage}>
+                <Image layout={"fill"} src={item.PathImage}/>
+            </div>
+            <div className={style.infoDiv}>
+                <div className={style.name}>
+                    {item.Name}
                 </div>
-                <div className={style.infoDiv}>
-                    <div className={style.name}>
-                        {item.Name}
-                    </div>
-                </div>
-            </button>
-        </Link>
+            </div>
+        </button>
     )
 }
