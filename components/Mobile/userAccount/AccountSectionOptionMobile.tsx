8a233a53865a6a94558cb4739ff495bd0db88165
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2022-10-16 18:42:04 -0300

    test

--- components/Mobile/userAccount/AccountSectionOptionMobile.tsx
+++ components/Mobile/userAccount/AccountSectionOptionMobile.tsx
@@ -1,14 +1,15 @@
 import style from "/styles/Mobile/UserAccount/accountSectionOption.module.css"
 import {AccountSections, ProviderAccountSections, AccountSectionContext} from "../../Providers/providerGlobal";
 import Image from "next/image";
-import {useContext, useState} from "react";
+import {useContext} from "react";
+import Link from "next/link";
 
 export default function AccountSectionOptionMobile({item}: { item: AccountSections }) {
     const accountSectionContext: ProviderAccountSections = useContext(AccountSectionContext)
     const handleSelectSection = (id: string) => {
         accountSectionContext.SelectSection(id)
     }
-    let [stateMyBussines, setStateMyBussines] = useState(false)
+    /*let [stateMyBussines, setStateMyBussines] = useState(false)*/
     /*useEffect(() => {
         accountSectionContext.ListAccountSection.forEach(item => {
             if (item.Name == "Mi Negocio") {
@@ -19,16 +20,18 @@ export default function AccountSectionOptionMobile({item}: { item: AccountSectio
     }, [accountSectionContext.ListAccountSection])*/
 
     return (
-        <button onClick={() => handleSelectSection(item.Id)}
-                className={item.State ? style.mainDivSelected : style.mainDiv}>
-            <div className={style.sizeImage}>
-                <Image layout={"fill"} src={item.PathImage}/>
-            </div>
-            <div className={style.infoDiv}>
-                <div className={style.name}>
-                    {item.Name}
+        <Link href={"userAccount"}>
+            <button onClick={() => handleSelectSection(item.Id)}
+                    className={item.State ? style.mainDivSelected : style.mainDiv}>
+                <div className={style.sizeImage}>
+                    <Image layout={"fill"} src={item.PathImage} alt={""}/>
                 </div>
-            </div>
-        </button>
+                <div className={style.infoDiv}>
+                    <div className={style.name}>
+                        {item.Name}
+                    </div>
+                </div>
+            </button>
+        </Link>
     )
 }
