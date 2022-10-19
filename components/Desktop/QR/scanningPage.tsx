e5825695e6f298356db5f95d736966b250f9ae68
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2022-10-19 16:13:37 -0300

    test

--- components/Desktop/QR/scanningPage.tsx
+++ components/Desktop/QR/scanningPage.tsx
@@ -3,13 +3,25 @@ import Image from "next/image";
 import {GlobalConst} from "../../../public/globalConst";
 import React, {useState} from 'react'
 import QrReader from 'react-qr-scanner'
+import Link from "next/link";
 
 const titleSection: string = "Escanear Codigo QR"
 
 export default function ScanningPage({scanningFunc}: { scanningFunc: Function }) {
     let [isOpenScanning, setIsOpenScanning] = useState(false)
     const handleOpenScanning = () => setIsOpenScanning(isOpenScanning = !isOpenScanning)
-    const handleScanning = () => scanningFunc()
+    let [resultScanning, setResultScanning] = useState(null)
+    const handleScanning = (e) => {
+        if (e != null) {
+            handleOpenScanning()
+            setResultScanning(resultScanning = e.text)
+            scanningFunc()
+        }
+    }
+    const handleError = ()=>{
+        handleOpenScanning()
+        setResultScanning("Error")
+    }
     return (
         <div className={style.backGroundBlue}>
             <div className={style.mainDiv}>
@@ -23,9 +35,15 @@ export default function ScanningPage({scanningFunc}: { scanningFunc: Function })
                     <div className={style.title}>
                         {titleSection}
                     </div>
+                    <Link href={resultScanning}>
+                        <a>{resultScanning}</a>
+                    </Link>
                     {
                         isOpenScanning ?
-                            <QrReader/>
+                            <QrReader
+                                delay={1000}
+                                onScan={handleScanning}
+                            onError={handleError}/>
                             :
                             <button onClick={handleOpenScanning} className={style.sizePlaceholderImage}>
                                 <Image layout={"fill"} src={GlobalConst.sourceImages.codigoQr}/>
