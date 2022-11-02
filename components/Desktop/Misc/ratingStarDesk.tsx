de96425a8f2aff4f885c5e2883f76f07818481fb
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2022-11-01 22:59:21 -0300

    test

--- components/Desktop/Misc/ratingStarDesk.tsx
+++ components/Desktop/Misc/ratingStarDesk.tsx
@@ -1,25 +1,29 @@
 import Image from "next/image";
 import {GlobalConst} from "../../../public/globalConst";
 import style from "/styles/Desktop/Misc/ratingStarDesk.module.css";
+
 const scaleRating: number = 5
 
 export default function RatingStarDesk({item}: { item: number }) {
+    const newArray = [1, 2, 3, 4, 5]
     return (
-        <div className={style.gridRatingStar}>
-            <div className={style.gridStars}>
+        < div
+            className={style.gridRatingStar}>
+            < div
+                className={style.gridStars}>
                 {
                     item == 0 ?
-                        [...Array(scaleRating)].map((e, index) =>
+                        newArray.map((e, index) =>
                             <div key={index} className={style.sizeStar}>
                                 <Image layout={"fill"}
                                        src={GlobalConst.sourceImages.ratingIndVoid} alt={""}/>
                             </div>
                         )
                         :
-                        [...Array(scaleRating)].map((e, index) =>
+                        newArray.map((e, index) =>
                             <div key={index} className={style.sizeStar}>
                                 <Image layout={"fill"}
-                                       src={(index + 1) <= item ? GlobalConst.sourceImages.ratingIndFull
+                                       src={e <= item ? GlobalConst.sourceImages.ratingIndFull
                                            : GlobalConst.sourceImages.ratingIndVoid} alt={""}/>
                             </div>
                         )
--- components/Mobile/Misc/ratingStarMob.tsx
+++ components/Mobile/Misc/ratingStarMob.tsx
@@ -4,19 +4,20 @@ import style from "/styles/Mobile/Misc/ratingStarMob.module.css";
 const scaleRating: number = 5
 
 export default function RatingStarMob({item}: { item: number }) {
+    const newArray = [1, 2, 3, 4, 5]
     return (
         <div className={style.gridRatingStar}>
             <div className={style.gridStars}>
                 {
                     item == 0 ?
-                        [...Array(scaleRating)].map((e, index) =>
+                        newArray.map((e, index) =>
                             <div key={index} className={style.sizeStar}>
                                 <Image layout={"fill"}
                                        src={GlobalConst.sourceImages.ratingIndVoid} alt={""}/>
                             </div>
                         )
                         :
-                        [...Array(scaleRating)].map((e, index) =>
+                        newArray.map((e, index) =>
                             <div key={index} className={style.sizeStar}>
                                 <Image layout={"fill"}
                                        src={(index + 1) <= item ? GlobalConst.sourceImages.ratingIndFull
