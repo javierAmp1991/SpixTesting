9bfa7e01598a21bbae26f763149992599065ad4d
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2022-11-01 22:50:53 -0300

    test

--- components/Desktop/Misc/ratingStarVar.tsx
+++ components/Desktop/Misc/ratingStarVar.tsx
@@ -2,20 +2,19 @@ import Image from "next/image";
 import {GlobalConst} from "../../../public/globalConst";
 import style from "/styles/Desktop/Misc/ratingStarDesk.module.css";
 
-const scaleRating: number = 4;
-
 export default function RatingStarVar({item, size}: { item: number, size: number }) {
+    const newArray = Array(5)
     return (
         <div className={style.gridRatingStar}>
             <div className={style.gridStars}>
                 {
                     item == 0 ?
-                        [...Array(scaleRating)].map((e, index) =>
+                        newArray.map((e, index) =>
                             <Image id={`${index}`} key={index} width={size} height={size}
                                    src={GlobalConst.sourceImages.ratingIndVoid} alt={""}/>
                         )
                         :
-                        [...Array(scaleRating)].map((e, index) =>
+                        newArray.map((e, index) =>
                             <Image id={`${index}`} key={index} width={size} height={size}
                                    src={(index + 1) <= item ? GlobalConst.sourceImages.ratingIndFull
                                        : GlobalConst.sourceImages.ratingIndVoid} alt={""}/>
