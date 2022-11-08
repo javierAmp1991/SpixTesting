79683b89b0275283d8ea76f8d8c3b42b8b7e9c4d
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2022-11-08 14:14:54 -0300

    test

--- components/Desktop/Site/TypeSites/Business/Restaurant/reservas.tsx
+++ components/Desktop/Site/TypeSites/Business/Restaurant/reservas.tsx
@@ -1,59 +1,122 @@
-import LayoutTitle from "./layoutTitle";
-import style from "/styles/Desktop/Site/TypeSite/Bussines/Restaurant/reservas.module.css"
+import style from "/styles/Desktop/Site/TypeSite/Bussines/Restaurant/reservas2.module.css"
 
-const listDates: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+const horario = [
+    {
+        Id: "horario001",
+        Hour: "08:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario002",
+        Hour: "09:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario003",
+        Hour: "10:00",
+        IsDisponible: false,
+    },
+    {
+        Id: "horario004",
+        Hour: "11:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario005",
+        Hour: "12:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario006",
+        Hour: "13:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario007",
+        Hour: "14:00",
+        IsDisponible: false,
+    },
+    {
+        Id: "horario008",
+        Hour: "15:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario009",
+        Hour: "16:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario0010",
+        Hour: "17:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario0011",
+        Hour: "18:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario0012",
+        Hour: "19:00",
+        IsDisponible: false,
+    },
+    {
+        Id: "horario0013",
+        Hour: "20:00",
+        IsDisponible: true,
+    },
+]
+const greenColor: string = "#10c010"
+const redColor: string = "#ff4a4a"
+const title: string = "Reservas"
+const horarioText: string = "Horario:"
+const disponibleTagText = "Reservas Disponibles"
+const noDisponibleTagText = "Reservas agotadas"
 
 export default function Reservas() {
     return (
-        <LayoutTitle title={"Reservas"}>
-            <div className={style.mainCont}>
-                <div className={style.mainDiv}>
-                    <div className={style.hour}>
-                        Horario: 08:00 - 16:00 / 18:00 -00:00
-                    </div>
-                    <div className={style.gridTags}>
-                        <div className={style.tagDisponible1}>
-                            Disponible
-                        </div>
-                        <div className={style.tagReservado1}>
-                            Reservado
-                        </div>
-                    </div>
-                    <div className={style.gridDates}>
-                        <div className={style.gridHoursLapse}>
-                            {
-                                [...Array(6)].map(e =>
-                                    <>
-                                        <div className={style.disponibleLapse}>
-                                            <div>08:00 hrs.</div>
-                                        </div>
-                                        <div className={style.noDisponibleLapse}>
-                                            <div>10:00 hrs.</div>
-                                        </div>
-                                    </>
-                                )
-                            }
-                        </div>
-                        <div className={style.gridHoursLapse}>
-                            {
-                                [...Array(6)].map(e =>
-                                    <>
-                                        <div className={style.disponibleLapse}>
-                                            <div>08:00 hrs.</div>
-                                        </div>
-                                        <div className={style.noDisponibleLapse}>
-                                            <div>10:00 hrs.</div>
-                                        </div>
-                                    </>
-                                )
-                            }
-                        </div>
-                    </div>
+        <div className={style.mainCont}>
+            <div className={style.mainDiv}>
+                <div className={style.title}>
+                    {title}
                 </div>
-                <button className={style.styleButton}>
-                    Reservar tu Mesa
-                </button>
+                <div className={style.hour}>
+                    {horarioText} de 08:00 a 20:00
+                </div>
+                <div className={style.gridTags}>
+                    <div className={style.tagDisponible1}>
+                        {disponibleTagText}
+                    </div>
+                    <div className={style.tagReservado1}>
+                        {noDisponibleTagText}
+                    </div>
+                    <div className={style.tagDisponible2}/>
+                    <div className={style.tagReservado2}/>
+                </div>
+                <div className={style.lineCircle}/>
+                <div className={style.gridHoursLapse}>
+                    {
+                        horario.map((item, index) =>
+                            <div key={item.Id}
+                                 style={{borderBottom: `2px solid ${item.IsDisponible ? greenColor : redColor}`}}
+                                 className={`${style.baseTag} 
+                                     ${(index + 1) % 2 == 0 ? style.leftSide : style.rightSide}`}>
+                                <div>{item.Hour} hrs.</div>
+                                <span style={{
+                                    border: `2px solid ${item.IsDisponible ? greenColor : redColor}`,
+                                    left: (index + 1) % 2 == 0 ? "calc(100% + 18px)" : "-34px"
+                                }}
+                                      className={style.baseCircle}/>
+                            </div>
+                        )
+                    }
+                </div>
+                <div className={style.lineCircle}/>
             </div>
-        </LayoutTitle>
+            <button className={style.styleButton}>
+                Reservar tu Mesa
+            </button>
+        </div>
     )
 }
--- components/Mobile/Site/TypeSites/Business/Restaurant/reservas.tsx
+++ components/Mobile/Site/TypeSites/Business/Restaurant/reservas.tsx
@@ -1,44 +1,124 @@
-import style from "/styles/Mobile/Site/TypeSite/Bussines/Restaurant/reservas.module.css"
+import style from "/styles/Mobile/Site/TypeSite/Bussines/Restaurant/reservas2.module.css"
 import LayoutTitleMobile from "./layoutTitleMobile";
 
-const listDates: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+const horario = [
+    {
+        Id: "horario001",
+        Hour: "08:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario002",
+        Hour: "09:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario003",
+        Hour: "10:00",
+        IsDisponible: false,
+    },
+    {
+        Id: "horario004",
+        Hour: "11:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario005",
+        Hour: "12:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario006",
+        Hour: "13:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario007",
+        Hour: "14:00",
+        IsDisponible: false,
+    },
+    {
+        Id: "horario008",
+        Hour: "15:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario009",
+        Hour: "16:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario0010",
+        Hour: "17:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario0011",
+        Hour: "18:00",
+        IsDisponible: true,
+    },
+    {
+        Id: "horario0012",
+        Hour: "19:00",
+        IsDisponible: false,
+    },
+    {
+        Id: "horario0013",
+        Hour: "20:00",
+        IsDisponible: true,
+    },
+]
+const greenColor: string = "#10c010"
+const redColor: string = "#ff4a4a"
+const title: string = "Reservas"
+const horarioText: string = "Horario:"
+const disponibleTagText = "Reservas Disponibles"
+const noDisponibleTagText = "Reservas agotadas"
 
 export default function ReservasMobile() {
     return (
-        <LayoutTitleMobile isOverflow={false} title={"Reservas"}>
-            <div className={style.mainCont}>
-                <div className={style.mainDiv}>
-                    <div className={style.hour}>
-                        Horario: 08:00 - 16:00 / 18:00 -00:00
-                    </div>
-                    <div className={style.gridTags}>
-                        <div className={style.tagDisponible1}>
-                            Disponible
-                        </div>
-                        <div className={style.tagReservado1}>
-                            Reservado
-                        </div>
-                    </div>
-                    <div className={style.beforeAfterUp}/>
-                    <div className={style.gridDates}>
-                        <div className={style.gridHoursLapse}>
-                            {
-                                [...Array(24)].map((e, index) =>
-
-                                    <div key={index}
-                                         className={index % 2 == 0 ? style.disponibleLapse : style.noDisponibleLapse}>
-                                        <div>0{index}:00 hrs.</div>
-                                    </div>
-                                )
-                            }
-                        </div>
-                    </div>
-                    <div className={style.beforeAfterDown}/>
+        <div className={style.mainCont}>
+            <div className={style.mainDiv}>
+                <div className={style.title}>
+                    {title}
                 </div>
-                <button className={style.styleButton}>
-                    Reservar tu Mesa
-                </button>
+                <div className={style.hour}>
+                    {horarioText} de 08:00 a 20:00
+                </div>
+                <div className={style.gridTags}>
+                    <div className={style.tagDisponible1}>
+                        {disponibleTagText}
+                    </div>
+                    <div className={style.tagReservado1}>
+                        {noDisponibleTagText}
+                    </div>
+                    <div className={style.tagDisponible2}/>
+                    <div className={style.tagReservado2}/>
+                </div>
+                <div className={style.lineCircle}/>
+                <div className={style.gridHoursLapse}>
+                    {
+                        horario.map((item, index) =>
+
+                            <div key={item.Id}
+                                 style={{borderBottom: `2px solid ${item.IsDisponible ? greenColor : redColor}`}}
+                                 className={`${style.baseTag} 
+                                     ${(index + 1) % 2 == 0 ? style.leftSide : style.rightSide}`}>
+                                <div>{item.Hour} hrs.</div>
+                                <span style={{
+                                    border: `2px solid ${item.IsDisponible ? greenColor : redColor}`,
+                                    left: (index + 1) % 2 == 0 ? "calc(100% + 8px)" : "-24px"
+                                }}
+                                      className={style.baseCircle}/>
+                            </div>
+                        )
+                    }
+                </div>
+                <div className={style.lineCircle}/>
             </div>
-        </LayoutTitleMobile>
+            <button className={style.styleButton}>
+                Reservar tu Mesa
+            </button>
+        </div>
     )
 }
