aa1004428ade50acb34ddd12e6dd583bcc1754f8
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2022-11-16 16:45:28 -0300

    test

--- components/Mobile/Site/TypeSites/Business/Beauty&Health/ourJobsMobile.tsx
+++ components/Mobile/Site/TypeSites/Business/Beauty&Health/ourJobsMobile.tsx
@@ -1,21 +1,18 @@
 import style from "/styles/Mobile/Site/TypeSite/Bussines/Beauty&Health/ourJobs.module.css"
 import Image from "next/image"
 import {PresentationCard} from "../../../../../../Class/Site/TypeSite/Business/restaurantClass";
-import {useContext, useState} from "react";
+import {useContext} from "react";
 import {HeaderDataBHContext} from "../../../../../Providers/Site/TypeSite/Business/Beauty&Health/beautyHealthProvider";
-import {GlobalId} from "../../../../../../public/globalConst";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const title: string = "Sobre nosotros"
 const listCortes: string[] =
     ["/images/corte4.jpeg", "/images/corte1.jpg", "/images/corte2.jpg", "/images/corte3.jpg", "/images/corte5.jpg"]
-const idPortal: string = GlobalId.globalIds.idPortal
 const seeGalery: string = "Ver Galeria"
+const title: string = "Nuestros Trabajos"
 
 export default function OurJobsMobile() {
     const info: PresentationCard = useContext(HeaderDataBHContext)
-    let [idx, setIdx] = useState(1)
     return (
         <div className={style.mainDiv}>
             {/* <div className={style.title}>
@@ -42,10 +39,4 @@ export default function OurJobsMobile() {
             </div>
         </div>
     )
-
-    function getStyle(num: number): string {
-        if (num == 2) return style.focus
-        else if (num == 0 || num == 4) return style.noFocus2
-        else return style.noFocus
-    }
 }
