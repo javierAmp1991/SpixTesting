07ddc4640e53319df0d4b24894d79e0c54f229c6
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2022-11-17 21:08:55 -0300

    test

--- components/Mobile/Site/TypeSites/Business/Beauty&Health/ourJobsMobile.tsx
+++ components/Mobile/Site/TypeSites/Business/Beauty&Health/ourJobsMobile.tsx
@@ -5,35 +5,38 @@ import {useContext} from "react";
 import {HeaderDataBHContext} from "../../../../../Providers/Site/TypeSite/Business/Beauty&Health/beautyHealthProvider";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import LayoutTitleCustomMobile from "../Misc/layoutTitleCustomMobile";
+import {LayoutTitleLinkProps} from "../../../../../../Class/Layouts/layoutClass";
 
 const seeGalery: string = "Ver Galeria"
 const title: string = "Nuestros Trabajos"
 
 export default function OurJobsMobile() {
     const info: PresentationCard = useContext(HeaderDataBHContext)
+    const layoutTitleProp: LayoutTitleLinkProps = {
+        Title: title
+    }
     return (
         <div className={style.mainDiv}>
-           {/* <div className={style.seeGalery}>
-                {seeGalery}
-            </div>*/}
+            <LayoutTitleCustomMobile item={layoutTitleProp}>
+                <>
+                    <div className={`${style.contSlider}`}>
+                        {
+                            info.SideImages.map((item, index) =>
+                                <div key={index} className={style.contImage}>
+                                    <div className={style.sizeImage}>
+                                        <Image layout={"fill"} src={item} alt={""}/>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
 
-            <div className={`${style.contSlider}`}>
-                {
-                    info.SideImages.map((item, index) =>
-                        <div key={index} className={`${style.contImage} ${getStyle(index)}`}>
-                            <div className={style.sizeImage}>
-                                <Image layout={"fill"} src={item} alt={""}/>
-                            </div>
-                        </div>
-                    )
-                }
-            </div>
+                    <div className={style.seeGalery}>
+                        {seeGalery}
+                    </div>
+                </>
+            </LayoutTitleCustomMobile>
         </div>
     )
-    function getStyle(num: number): string {
-        if (num == 0) return style.first
-        else if (num == 1) return style.second
-        else if (num == 2) return style.third
-        else return style.four
-    }
 }
